193da28976cc358c77098c1925f07a16824708ce
Eric <ericsouzapalma123@gmail.com>
2025-02-07 08:44:40 -0300

    login

--- View/login.php
+++ View/login.php
@@ -1,3 +1,7 @@
-<?php
-
-require_once "";
+<h2>Login</h2>
+<form method="POST" action="index.php?route=login">
+    <input type="text" name="username" placeholder="Usuário" required>
+    <input type="password" name="password" placeholder="Senha" required>
+    <button type="submit">Entrar</button>
+</form>
+<a href="index.php?route=register">Criar Conta</a>
