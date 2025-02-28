16f34c1f8855caa016d253d76246fcf4e5a28506
T1NT4 <thiagogabriel.osil@gmail.com>
2025-02-28 08:12:18 -0300

    mudei os titulos das paginas

--- feedback.php
+++ feedback.php
@@ -23,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PÁGINA DO USUÁRIO - Reverdecer</title>
+    <title>FEEDBACK - Reverdecer</title>
     <link rel="stylesheet" href="View/estilo.css">
     <script defer src="app.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
--- registerResiduos.php
+++ registerResiduos.php
@@ -50,7 +50,7 @@ if (!empty($_POST)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="View/estilo.css">
-    <title>FORMULÁRIO</title>
+    <title>FORMULÁRIO - Reverdecer</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
 
